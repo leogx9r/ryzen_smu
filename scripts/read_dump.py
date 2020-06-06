d6c9dcce8e14cc6ec36e2fb3317bb0989a647841
Leonardo Gates <leogatesx9r@protonmail.com>
2020-06-06 15:52:44 +0000

    Added helper script to read floats of PM table

---
+++ scripts/read_dump.py
@@ -0,0 +1,45 @@
+#!/bin/python
+
+import os
+import sys
+import struct
+
+def read_pm():
+    with open("dump.bin", "rb") as fp:
+        content = fp.read()
+        fp.close()
+    return content
+
+def read_float(buffer, offset):
+    return struct.unpack("@f", buffer[offset:(offset + 4)])[0]
+
+def read_double(buffer, offset):
+    return struct.unpack("@d", buffer[offset:(offset + 8)])[0]
+
+def read_int(buffer, offset):
+    return struct.unpack("@I", buffer[offset:(offset + 4)])[0]
+
+def dump_float():
+    c = read_pm()
+    size = len(c)
+
+    i = 0
+    while i < size:
+        v = read_float(c, i)
+        print("0x{:04X} -> {:8.6f}".format(i, v))
+        i = i + 4
+
+def dump_double():
+    c = read_pm()
+    size = len(c)
+
+    i = 0
+    while i < size:
+        v = read_double(c, i)
+        print("0x{:04X} -> {:8.6f}".format(i, v))
+        i = i + 8
+
+
+dump_float()
+#dump_double()
+#test_pwr()
